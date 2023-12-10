6983e3375408dc19599264ff90a0c58efb48a982
Daria <dashakoshelek@gmail.com>
2023-12-10 20:45:07 +0300

    appointment

---
+++ vetclinic/Appointment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vetclinic
+{
+    internal class Appointment
+    {
+        public static int CountID = 0;
+        public int AppointmentID;
+        public DateTime AppointmentDate { get; set; }
+
+        public Appointment(DateTime appointmentDate) 
+        {
+            AppointmentDate = appointmentDate;
+        }
+    }
+}
