629fa4ff9321a1bc5c2c9414168223fa53129034
Daria <dashakoshelek@gmail.com>
2023-12-19 17:10:22 +0300

    пространства имен

--- vetclinic/Managers/PetsManager.cs
+++ vetclinic/Managers/PetsManager.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using vetclinic.Bases;
 
-namespace vetclinic.Bases
+namespace vetclinic.Managers
 {
     internal class PetsManager
     {
